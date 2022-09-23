7825b0725c36ab051ccd15dfa75a7e086ff09cd9
Efty <efty131331@gmail.com>
2022-09-23 10:35:09 +0600

    2nd commit

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // Text to Binary
 const inputElement = document.querySelector(".input-text");
 const outputElement = document.getElementById("Output-binary");
-// console.log(input,output);
-
-inputElement.addEventListener("input", (event)=>{
-    let inputText = event.target.value; 
+function encode(){
+    console.log(23324);
+    let inputText = inputElement.value;
     let outputArray = [];
     for (var i = 0; i < inputText.length; i++) {
-        outputArray.push(inputText.charCodeAt(i).toString(2));
-    }
-    outputElement.innerHTML = outputArray.join(" ");
-});
+    outputArray.push(inputText.charCodeAt(i).toString(2));
+     };
+     outputElement.innerHTML = outputArray.join(" ");
+}
+// Binary to Text
+ const decode = () => {
+    const BinaryInput = document.querySelector(".input-binary").value;
+    const res = BinaryInput.split(' ').map(b => parseInt(b,2)).map(num => String.fromCharCode(num)).join('');
+    document.getElementById("Output-text").innerText = res;
+ }
