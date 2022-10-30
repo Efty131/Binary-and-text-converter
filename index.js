796dc50193530f9f10ba8558dbfeed613de806d1
Efty <efty131331@gmail.com>
2022-10-30 13:23:05 +0600

    m/s to km/h converter added

--- index.js
+++ index.js
@@ -2,17 +2,23 @@
 const inputElement = document.querySelector(".input-text");
 const outputElement = document.getElementById("Output-binary");
 function encode(){
-    console.log(23324);
     let inputText = inputElement.value;
     let outputArray = [];
     for (var i = 0; i < inputText.length; i++) {
     outputArray.push(inputText.charCodeAt(i).toString(2));
      };
      outputElement.innerHTML = outputArray.join(" ");
-}
+};
 // Binary to Text
  const decode = () => {
     const BinaryInput = document.querySelector(".input-binary").value;
     const res = BinaryInput.split(' ').map(b => parseInt(b,2)).map(num => String.fromCharCode(num)).join('');
     document.getElementById("Output-text").innerText = res;
+ };
+ // m/s to km/h
+ const km = () => {
+   const ms = document.querySelector(".MS").value;
+   const outputKmh = document.getElementById("Output-kmh");
+   const kmh = ms * 3.6;
+   outputKmh.innerHTML = kmh;
  }
